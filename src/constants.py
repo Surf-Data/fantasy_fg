ef83c4b832cd28776c4dcc65d56aa95f6e0e9c90
Surf-Data <surfdatasolutions@gmail.com>
2023-05-31 11:20:44 -0500

    added constants

---
+++ src/constants.py
@@ -0,0 +1,23 @@
+POSITION_MAP = {
+    0: 'C',
+    1: '1B',
+    2: '2B',
+    3: '3B',
+    4: 'SS',
+    5: 'OF',
+    #6: '2B/SS',
+    #7: '1B/3B',
+    #8: 'LF',
+    #9: 'CF',
+    #10: 'RF',
+    11: 'DH',
+    #12: 'UTIL',
+    #13: 'P',
+    14: 'SP',
+    15: 'RP',
+    #16: 'BE',
+    #17: 'IL',
+    #19: 'IF', # 1B/2B/SS/3B
+    # reverse TODO
+    # 18, 21, 22 have appeared but unknown what position they correspond to
+}
